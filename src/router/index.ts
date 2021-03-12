64788fe47e90fa5ecbe42fa9464d7194d8285460
jawa0919 <jawa0919@163.com>
2021-03-12 18:52:15 +0800

    jest学习

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
+import VueRouter, { NavigationGuardNext, Route, RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
@@ -8,7 +8,7 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
     path: "/about",
@@ -17,14 +17,30 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/Demo1",
+    name: "Demo1",
+    component: () => import("../views/Demo1.vue"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
+});
+
+router.beforeEach((to: Route, from: Route, next: NavigationGuardNext) => {
+  console.log("🚀 ~ file: index.ts ~ line 31 ~ router.beforeEach ~ to", to);
+  console.log("🚀 ~ file: index.ts ~ line 31 ~ router.beforeEach ~ from", from);
+  next();
+});
+
+router.afterEach((to: Route, from: Route) => {
+  console.log("🚀 ~ file: index.ts ~ line 31 ~ router.afterEach ~ to", to);
+  console.log("🚀 ~ file: index.ts ~ line 31 ~ router.afterEach ~ from", from);
 });
 
 export default router;
